39438cc49abe91118a7637ad1db8fcd7bb900566
Juarez Bochi <jbochi@gmail.com>
2012-05-02 11:25:12 -0300

    balancer: Making names consistent: playlists instead of streams

--- tests/test_balancer.py
+++ tests/test_balancer.py
@@ -6,11 +6,11 @@ def test_balancer_returns_active_server_if_its_the_only_one():
 	paths = {PATH: [SERVER]}
 	b = Balancer()
 	b.update(paths)
-	active_streams = list(b.actives)
-	assert 1 == len(active_streams)
-	assert PATH == active_streams[0].path
-	assert SERVER == active_streams[0].server
-	assert SERVER + PATH == str(active_streams[0])
+	active_playlists = list(b.actives)
+	assert 1 == len(active_playlists)
+	assert PATH == active_playlists[0].path
+	assert SERVER == active_playlists[0].server
+	assert SERVER + PATH == str(active_playlists[0])
 
 def test_balancer_supports_multiple_paths():
 	PATH1 = '/path1'
