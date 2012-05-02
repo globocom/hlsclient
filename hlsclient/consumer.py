72d77bbf1955e67451bf323162fb3e0ecd19884c
Juarez Bochi <jbochi@gmail.com>
2012-05-02 18:36:37 -0300

    consumer: Saves key locally

--- hlsclient/consumer.py
+++ hlsclient/consumer.py
@@ -1,6 +1,12 @@
 import m3u8
 
 def consume(m3u8_path, destination_path):
-	"Receives a m3u8 path and copies all files to a local path"
-	playlist = m3u8.M3U8()
-	playlist.load(m3u8_path)
+    "Receives a m3u8 path and copies all playlist files to a local path"
+    playlist = m3u8.model.M3U8()
+    playlist.load(m3u8_path)
+    if playlist.key:
+        download_to_file(playlist.key.uri, destination_path)
+
+def download_to_file(uri, local_path):
+    "Retrives the file if it does not exist locally"
+    pass
--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,13 +1,40 @@
+from collections import namedtuple
 import m3u8
-from hlsclient.consumer import consume
+import hlsclient.consumer
 
+Key = namedtuple('Key', 'uri')
 
-def test_consume_loads_path(monkeypatch):
-	called_args = []
-	class FakeM3U8(object):
-		def load(self, url):
-			called_args.append(url)
+class BaseFakeM3U8(object):
+    def load(self, uri):
+        pass
 
-	monkeypatch.setattr(m3u8, 'M3U8', FakeM3U8)
-	consume('uri', '/path')
-	assert ['uri'] == called_args
+    @property
+    def key(self):
+        return Key(uri=None)
+
+def test_if_consume_loads_path(monkeypatch):
+    called_args = []
+    class FakeM3U8(BaseFakeM3U8):
+        def load(self, url):
+            called_args.append(url)
+
+    monkeypatch.setattr(m3u8.model, 'M3U8', FakeM3U8)
+    hlsclient.consumer.consume('uri', '/path')
+    assert ['uri'] == called_args
+
+def test_if_consume_downloads_key_file(monkeypatch):
+    class FakeM3U8(BaseFakeM3U8):
+        @property
+        def key(self):
+            return Key('/key')
+    monkeypatch.setattr(m3u8.model, 'M3U8', FakeM3U8)
+
+    called_args = []
+    def fake_download_to_file(uri, local_path):
+        assert '/key' == uri
+        assert local_path
+        called_args.append(uri)
+    monkeypatch.setattr(hlsclient.consumer, 'download_to_file',
+        fake_download_to_file)
+    hlsclient.consumer.consume('uri', '/path')
+    assert 1 == len(called_args)
