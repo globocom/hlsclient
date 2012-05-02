46bd8bccd0a01dd4842aea0406b9e1781dd41229
Juarez Bochi <jbochi@gmail.com>
2012-05-02 11:03:47 -0300

    balancer: guarantees that multiple paths are supported

--- tests/test_balancer.py
+++ tests/test_balancer.py
@@ -11,3 +11,15 @@ def test_balancer_returns_active_server_if_its_the_only_one():
 	assert PATH == active_streams[0].path
 	assert SERVER == active_streams[0].server
 	assert SERVER + PATH == str(active_streams[0])
+
+def test_balancer_supports_multiple_paths():
+	PATH1 = '/path1'
+	PATH2 = '/path2'
+	SERVER = 'server'
+	paths = {PATH1: [SERVER], PATH2: [SERVER]}
+	b = Balancer()
+	b.update(paths)
+	paths = sorted(s.path for s in b.actives)
+	assert 2 == len(paths)
+	assert PATH1 == paths[0]
+	assert PATH2 == paths[1]
