694fe54552ae53854d8d3ac9fccf620b38d5b6c3
Juarez Bochi <jbochi@gmail.com>
2012-05-03 12:01:35 -0300

    Implemented main loop

---
+++ hlsclient/__main__.py
@@ -0,0 +1,48 @@
+import ConfigParser
+import logging
+import time
+
+from balancer import Balancer
+from discover import discover
+from consumer import consume
+
+def load_config(path='config.ini'):
+    config = ConfigParser.RawConfigParser()
+    with open(path) as f:
+        config.readfp(f)
+    return config
+
+def main():
+    logging.basicConfig()
+    logger = logging.getLogger('hls-client')
+    logger.setLevel(logging.DEBUG)
+    logger.debug('HLS CLIENT Started')
+
+    config = load_config()
+    destination = config.get('hlsclient', 'destination')
+    logger.debug('Config loaded')
+
+    balancer = Balancer()
+
+    while True:
+        paths = discover(config)
+        logger.info(u'Discovered the following paths: %s' % paths.items())
+        balancer.update(paths)
+
+        for resource in balancer.actives:
+            try:
+                logger.debug('Consuming %s' % resource)
+                resource_path = str(resource)
+                modified = consume(resource_path, destination)
+                if modified:
+                    logger.info('Notifying content modified: %s' % resource)
+                    balancer.notify_modified(resource.server, resource.path)
+                else:
+                    logger.info('Content not modified: %s' % resource)
+            except (IOError, OSError) as err:
+                logger.warning(u'Notifying error!')
+                balancer.notify_error(resource.server, resource.path)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
