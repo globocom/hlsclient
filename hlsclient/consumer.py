9bd1d01e4a24c0fba06ccd760e4d8cac43ebbd10
Igor Sobreira <igor@igorsobreira.com>
2012-05-03 16:39:21 -0300

    consumer: save m3u8 file on disk if any segment from it was saved. And save segments in the same directory of m3u8

--- hlsclient/consumer.py
+++ hlsclient/consumer.py
@@ -1,31 +1,59 @@
-import m3u8
 import os
+import errno
 import urllib
 from urlparse import urlparse
 
+import m3u8
 
-def consume(m3u8_path, destination_path):
-    "Receives a m3u8 path and copies all playlist files to a local path"
-    playlist = m3u8.load(m3u8_path)
+def consume(m3u8_uri, destination_path):
+    '''
+    Given a ``m3u8_uri``, downloads all files to disk
+    The remote path structure is maintained under ``destination_path``
 
-    files_to_download = []
+    '''
+    playlist = m3u8.load(m3u8_uri)
+
+    full_path = build_full_path(destination_path, m3u8_uri)
+
+    resources = collect_resources_to_download(playlist)
+    modified = download_resources_to_files(resources, full_path)
+
+    if modified:
+        save_m3u8(playlist, m3u8_uri, full_path)
+
+    return modified
+
+def build_full_path(destination_path, m3u8_uri):
+    url_path = urlparse(m3u8_uri).path
+    intermediate_path = os.path.dirname(url_path[1:])  # ignore first "/"
+    full_path = os.path.join(destination_path, intermediate_path)
+    ensure_directory_exists(full_path)
+    return full_path
+
+def ensure_directory_exists(directory):
+    try:
+        os.makedirs(directory)
+    except OSError as error:
+        if error.errno != errno.EEXIST:
+            raise
+
+def collect_resources_to_download(playlist):
+    resources = []
     if playlist.key:
-        files_to_download.append(playlist.key.uri)
+        resources.append(playlist.key.uri)
+    resources.extend([segment.uri for segment in playlist.segments])
+    return resources
 
-    files_to_download.extend([segment.uri for segment in playlist.segments])
-    downloaded = [download_to_file(f, destination_path) for f
-        in files_to_download]
-    return any(downloaded)
+def download_resources_to_files(resources, destination_path):
+    return any([download_to_file(r, destination_path) for r in resources])
+
+def save_m3u8(playlist, m3u8_uri, full_path):
+    filename = os.path.join(full_path, os.path.basename(m3u8_uri))
+    playlist.dump(filename)
 
 def download_to_file(uri, local_path):
     "Retrives the file if it does not exist locally"
-    remote_path = urlparse(uri).path
-    basename = os.path.basename(remote_path)
-    localpath = os.path.join(local_path, basename)
-
-    if not os.path.exists(local_path):
-        os.makedirs(local_path)
-
+    localpath = os.path.join(local_path, os.path.basename(uri))
     if not os.path.exists(localpath):
         urllib.urlretrieve(url=uri, filename=localpath)
         return True
--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -10,6 +10,8 @@ Key = namedtuple('Key', 'uri')
 Segment = namedtuple('Segment', 'uri')
 
 class BaseFakeM3U8(object):
+    _m3u8_saved_path = None
+
     @property
     def key(self):
         return None
@@ -18,17 +20,20 @@ class BaseFakeM3U8(object):
     def segments(self):
         return []
 
-def test_if_consume_loads_path(monkeypatch):
+    def dump(self, path):
+        self._m3u8_saved_path = path
+
+def test_if_consume_loads_path(monkeypatch, tmpdir):
     called_args = []
     def fake_load(url):
         called_args.append(url)
         return BaseFakeM3U8()
 
     monkeypatch.setattr(m3u8, 'load', fake_load)
-    hlsclient.consumer.consume('m3u8', '/local_path')
+    hlsclient.consumer.consume('m3u8', str(tmpdir.join('local_path')))
     assert ['m3u8'] == called_args
 
-def test_if_consume_downloads_key_file(monkeypatch):
+def test_if_consume_downloads_key_file(monkeypatch, tmpdir):
     class FakeM3U8(BaseFakeM3U8):
         @property
         def key(self):
@@ -38,33 +43,35 @@ def test_if_consume_downloads_key_file(monkeypatch):
     called_args = []
     def fake_download_to_file(uri, local_path):
         assert '/key' == uri
-        assert local_path == '/local_path'
         called_args.append(uri)
         return True
     monkeypatch.setattr(hlsclient.consumer, 'download_to_file',
         fake_download_to_file)
-    assert hlsclient.consumer.consume('m3u8', '/local_path')
+    assert hlsclient.consumer.consume('m3u8', str(tmpdir.join('local_path')))
     assert 1 == len(called_args)
 
-def test_if_consume_downloads_segments(monkeypatch):
+def test_if_consume_downloads_segments_and_saves_on_the_correct_path(monkeypatch, tmpdir):
+    REMOTE_CHUNKS = ['http://server/another/remote/location/chunk1.ts',
+                     'http://server/another/remote/location/chunk2.ts']
+
     class FakeM3U8(BaseFakeM3U8):
         @property
         def segments(self):
-            return [Segment(uri='/path1'),
-                    Segment(uri='/path2')]
+            return [Segment(uri=uri) for uri in REMOTE_CHUNKS]
     monkeypatch.setattr(m3u8, 'load', lambda _: FakeM3U8())
 
     called_args = []
     def fake_download_to_file(uri, local_path):
-        assert local_path == '/path'
+        assert local_path == str(tmpdir.join('local_path', 'remote', 'path'))
         called_args.append(uri)
         return True
     monkeypatch.setattr(hlsclient.consumer, 'download_to_file',
         fake_download_to_file)
-    assert hlsclient.consumer.consume('m3u8', '/path')
-    assert ['/path1', '/path2'] == called_args
 
-def test_if_consume_returns_false_if_there_is_no_new_file(monkeypatch):
+    assert hlsclient.consumer.consume('http://server/remote/path/m3u8', str(tmpdir.join('local_path')))
+    assert REMOTE_CHUNKS == called_args
+
+def test_if_consume_returns_false_if_there_is_no_new_file(monkeypatch, tmpdir):
     class FakeM3U8(BaseFakeM3U8):
         @property
         def segments(self):
@@ -76,7 +83,7 @@ def test_if_consume_returns_false_if_there_is_no_new_file(monkeypatch):
         return False
     monkeypatch.setattr(hlsclient.consumer, 'download_to_file',
         fake_download_to_file)
-    assert not hlsclient.consumer.consume('m3u8', '/path')
+    assert not hlsclient.consumer.consume('m3u8', str(tmpdir.join('local_path')))
 
 def test_if_download_to_file_saves_the_file_with_correct_path(monkeypatch):
     called_args = []
@@ -104,12 +111,47 @@ def test_if_download_to_file_does_nothing_if_file_already_exists(monkeypatch):
     hlsclient.consumer.download_to_file(SEGMENT_URI, '/tmp/')
     assert 0 == len(called_args)
 
-def test_if_download_to_file_creates_intermediate_directories():
-    path_with_intermediate_directories = '/tmp/inter/go/'
-    SEGMENT_URI = 'http://example.com/path/subpath/chunk.ts'
+def test_if_download_to_file_creates_intermediate_directories(monkeypatch, tmpdir):
+    monkeypatch.setattr(m3u8, 'load', lambda _: BaseFakeM3U8())
+    monkeypatch.setattr(hlsclient.consumer, 'download_to_file', lambda *args: True)
 
-    shutil.rmtree(path_with_intermediate_directories, ignore_errors=True)
-    hlsclient.consumer.download_to_file(SEGMENT_URI,
-            path_with_intermediate_directories)
+    destination_path = tmpdir.join('subdir1', 'subdir2')
+    hlsclient.consumer.consume('http://server.com/live/stream.m3u8', str(destination_path))
 
-    assert os.path.exists(path_with_intermediate_directories)
+    expected_path = destination_path.join('live')
+
+    assert expected_path.check()
+
+def test_if_consume_saves_m3u8_file_if_new_segment_saved(monkeypatch, tmpdir):
+    class FakeM3U8(BaseFakeM3U8):
+        @property
+        def segments(self):
+            return [Segment(uri='/path1'),
+                    Segment(uri='/path2')]
+
+    fake_m3u8 = FakeM3U8()
+
+    monkeypatch.setattr(m3u8, 'load', lambda _: fake_m3u8)
+    monkeypatch.setattr(hlsclient.consumer, 'download_to_file', lambda *args: True)
+
+    hlsclient.consumer.consume('http://server/remote/path/file.m3u8', str(tmpdir.join('local_path')))
+    # ignore existing
+    hlsclient.consumer.consume('http://server/remote/path/file.m3u8', str(tmpdir.join('local_path')))
+
+    assert tmpdir.join('local_path', 'remote', 'path', 'file.m3u8') == fake_m3u8._m3u8_saved_path
+
+def test_if_consume_does_not_save_m3u8_file_if_no_segment_saved(monkeypatch, tmpdir):
+    class FakeM3U8(BaseFakeM3U8):
+        @property
+        def segments(self):
+            return [Segment(uri='/path1'),
+                    Segment(uri='/path2')]
+
+    fake_m3u8 = FakeM3U8()
+
+    monkeypatch.setattr(m3u8, 'load', lambda _: fake_m3u8)
+    monkeypatch.setattr(hlsclient.consumer, 'download_to_file', lambda *args: False)
+
+    hlsclient.consumer.consume('m3u8', str(tmpdir.join('local_path')))
+
+    assert not fake_m3u8._m3u8_saved_path
