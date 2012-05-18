82c23433bb12fbbdef8c95da6e6f62224d558779
Hugo Lopes Tavares <hltbra@gmail.com>
2012-05-18 16:53:38 -0300

    setup.py: Add author, dependencies, and bump version

--- setup.py
+++ setup.py
@@ -8,20 +8,15 @@ setup(
     name='hlsclient',
     description='Client to download all files from HLS streams',
     long_description=README,
+    author='Globo.com',
     url='https://github.com/globocom/hlsclient',
-    version='0.0',
+    version='0.1.0',
     zip_safe=False,
     packages=[
         'hlsclient',
-        'hlsclient.discover'
         ],
     install_requires=[
         'requests==0.11.2',
-        'm3u8==0.0',
-        'fms==0.0.1',
-        ],
-    dependency_links=[
-        'http://github.com/globocom/m3u8/tarball/master#egg=m3u8-0.0',
-        'http://github.com/jbochi/fms/tarball/master#egg=fms-0.0.1',
+        'm3u8==0.1.0',
         ],
 )
