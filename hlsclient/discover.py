bd8fbe15f1e1fef71629bc1e9d8d7c839d416744
Hugo Lopes Tavares <hltbra@gmail.com>
2012-05-21 11:04:33 -0300

    discover: Implement multiple bitrate discovering (issue #11)

--- hlsclient/discover.py
+++ hlsclient/discover.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+import urlparse
 from collections import namedtuple
 
 
@@ -8,24 +9,43 @@ Server = namedtuple('Server', 'server port')
 
 def discover(config):
     '''
-    Receives the extra configuration parameters from [discover] section
-
     Returns a dictionary with format:
 
-      {'/path1.m3u8': ['server1', 'server2'],
-       '/path2.m3u8': ['server3', 'server4', 'server5']}
+      {'/playlist1.m3u8': [Server('server1'), Server('server2')],
+       '/playlist2.m3u8': [Server('server2'), Server('server3'), Server('server4')]}
 
     '''
     api_url = config.get('discover', 'api_url')
     result = {}
     info = get_info_from_url(api_url)
     for info in get_info_from_url(api_url):
-        m3u8 = info['m3u8']
-        servers = [Server(server=server, port=80) for server in info['servers']]
-        result[m3u8] = servers
+        if not info['needs_index']:
+            _append_m3u8_without_mbr_to(result, info)
+        else:
+            _append_m3u8_with_mbr_to(result, info)
     return result
 
 
 def get_info_from_url(url):
     # FIXME: implement error checking
     return json.load(urllib.urlopen(url))['actives']
+
+def _append_m3u8_without_mbr_to(result, info):
+    playlist = info['m3u8']
+    result[playlist] = _build_servers(m3u8['servers'])
+
+
+def _append_m3u8_with_mbr_to(result, info):
+    bitrates = info['bitrates']
+    for m3u8 in bitrates:
+        playlist = m3u8['m3u8']
+        result[playlist] = _build_servers(m3u8['servers'])
+
+def _build_servers(servers):
+    result = []
+    for server in servers:
+        parsed_url = urlparse.urlparse(server)
+        server_url = '{scheme}://{hostname}'.format(scheme=parsed_url.scheme, hostname=parsed_url.hostname)
+        port = parsed_url.port
+        result.append(Server(server=server_url, port=port))
+    return result
--- tests/test_discover.py
+++ tests/test_discover.py
@@ -15,16 +15,47 @@ api_url = http://localhost:4422/tests.m3u8
         # m3u8 sample got from http://webme.ws/live-docs/thorp.html
         return [{
             'm3u8': '/hls-without-mbr.m3u8',
-            'servers': ['serv1.com', 'serv2.com'],
+            'servers': ['http://serv1.com', 'http://serv2.com'],
             'bitrates': [],
             'needs_index': False,
         }]
 
     monkeypatch.setattr(hlsclient.discover, 'get_info_from_url', fake_get_info_from_url)
 
+    playlist = '/hls-without-mbr.m3u8'
+    servers = [Server(server='http://serv1.com', port=80), Server(server='http://serv2.com', port=80)]
+    assert {playlist: servers} == discover(config)
+
+
+def test_discovers_simple_m3u8_from_api_url_in_config(monkeypatch):
+    sample_config = """[discover]
+api_url = http://localhost:4422/mbr-tests.m3u8
+"""
+    config = ConfigParser.RawConfigParser()
+    config.readfp(io.BytesIO(sample_config))
+
+    def fake_get_info_from_url(url):
+        # m3u8 sample got from http://webme.ws/live-docs/thorp.html
+        return [{
+            'm3u8': '/hls-with-mbr.m3u8',
+            'servers': [],
+            'bitrates': [
+                {'m3u8': '/hls100.m3u8', 'servers': ['http://serv1.com:80', 'http://serv2.com:1234'], 'bandwitdh': 100},
+                {'m3u8': '/hls200.m3u8', 'servers': ['http://serv1.com:81', 'http://serv2.com:2345'], 'bandwitdh': 200},
+                {'m3u8': '/hls300.m3u8', 'servers': ['http://serv1.com:82', 'http://serv2.com:3456'], 'bandwitdh': 300}
+            ],
+            'needs_index': True,
+        }]
+
+    monkeypatch.setattr(hlsclient.discover, 'get_info_from_url', fake_get_info_from_url)
+
     m3u8_path = '/hls-without-mbr.m3u8'
-    servers = [Server(server='serv1.com', port=80), Server(server='serv2.com', port=80)]
-    assert {m3u8_path: servers} == discover(config)
-
-
-
+    low_playlist = '/hls100.m3u8'
+    mid_playlist = '/hls200.m3u8'
+    high_playlist = '/hls300.m3u8'
+    expected_result = {
+        low_playlist: [Server('http://serv1.com', port=80), Server('http://serv2.com', 1234)],
+        mid_playlist: [Server('http://serv1.com', port=81), Server('http://serv2.com', 2345)],
+        high_playlist: [Server('http://serv1.com', port=82), Server('http://serv2.com', 3456)],
+    }
+    assert expected_result == discover(config)
