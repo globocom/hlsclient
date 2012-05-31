294c7a0f1c9dfd705ae16ea3e5d97da80f835efc
Juarez Bochi <jbochi@gmail.com>
2012-05-31 10:50:28 -0300

    cleaner: Cleaning only m3u8, bin, and ts files (issue #3)

--- hlsclient/cleaner.py
+++ hlsclient/cleaner.py
@@ -2,6 +2,10 @@ import os
 import time
 import shutil
 import logging
+import re
+
+
+FILES_PATTERN = re.compile('.+\.(ts|m3u8|bin)$')
 
 def path_age(path):
     atime = os.path.getatime(path)
@@ -14,10 +18,14 @@ def filter_old_paths(basepath, paths, maxage):
         if os.path.exists(fullpath) and path_age(fullpath) > maxage:
             yield fullpath
 
+def filter_old_files(basepath, paths, maxage):
+    old_paths = filter_old_paths(basepath, paths, maxage)
+    return (f for f in old_paths if FILES_PATTERN.match(f))
+
 def clean(path, maxage):
     logging.debug("Cleaning {path} (maxage = {maxage}s)".format(path=path, maxage=maxage))
     for root, dirs, files in os.walk(path):
-        for filename in filter_old_paths(root, files, maxage):
+        for filename in filter_old_files(root, files, maxage):
             logging.debug("Removing old file {path}".format(path=filename))
             os.remove(filename)
         for directory in filter_old_paths(root, dirs, maxage):
--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -7,21 +7,21 @@ SECONDS = 1
 
 def test_should_remove_old_files(tmpdir):
     tmpdir = str(tmpdir)
-    old_fnames = 'old1.txt old2.txt old3.txt'.split()
-    fresh_files = 'a.txt b.txt c.txt'.split()
+    old_fnames = 'old1.ts old2.m3u8 old3.bin d.txt'.split()
+    fresh_files = 'a.ts b.ts c.ts'.split()
     create_old_files(tmpdir, old_fnames, 60 * SECONDS)
     create_fresh_files(tmpdir, fresh_files)
 
     clean(tmpdir, 50 * SECONDS)
-    assert os.listdir(tmpdir) == fresh_files
+    assert os.listdir(tmpdir) == fresh_files + ['d.txt']
 
 
 def test_should_remove_old_files_and_directories(tmpdir):
     tmpdir = str(tmpdir)
     nested_dir1 = os.path.join(tmpdir, 'dir1')
     nested_dir2 = os.path.join(nested_dir1, 'dir2')
-    old_fnames = 'old1.txt old2.txt old3.txt'.split()
-    fresh_files = 'a.txt b.txt c.txt'.split()
+    old_fnames = 'old1.ts old2.ts old3.ts'.split()
+    fresh_files = 'a.ts b.ts c.ts'.split()
     os.makedirs(nested_dir2)
 
     create_old_files(nested_dir2, old_fnames, 60 * SECONDS)
@@ -36,16 +36,16 @@ def test_should_remove_old_files_and_directories(tmpdir):
 
 def test_ensure_removes_are_based_on_access_time(tmpdir):
     tmpdir = str(tmpdir)
-    old_fnames = 'old1.txt old2.txt old3.txt'.split()
-    fresh_files = 'a.txt b.txt c.txt'.split()
+    old_fnames = 'old1.ts old2.ts old3.ts'.split()
+    fresh_files = 'a.ts b.ts c.ts'.split()
     create_old_files(tmpdir, old_fnames, 60 * SECONDS)
     create_fresh_files(tmpdir, fresh_files)
 
-    with open(os.path.join(tmpdir, 'old1.txt')) as f:
+    with open(os.path.join(tmpdir, 'old1.ts')) as f:
         f.read()
 
     clean(tmpdir, 50 * SECONDS)
-    assert os.listdir(tmpdir) == fresh_files + ['old1.txt']
+    assert os.listdir(tmpdir) == fresh_files + ['old1.ts']
 
 
 def create_old_files(destination, files, modification_timedelta):
