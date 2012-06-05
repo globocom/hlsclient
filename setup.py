8f66152830e086e1d93648e267568508dd83656a
Juarez Bochi <jbochi@gmail.com>
2012-06-05 09:55:57 -0300

    Bump to 0.1.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=README,
     author='Globo.com',
     url='https://github.com/globocom/hlsclient',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
     include_package_data=True,
     packages=[
