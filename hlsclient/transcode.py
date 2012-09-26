80c9f3f8c929b49ff618429bb6aa1908ced16f60
Juarez Bochi <jbochi@gmail.com>
2012-09-26 19:23:35 -0300

    transcode: Add video support

--- hlsclient/transcode.py
+++ hlsclient/transcode.py
@@ -6,11 +6,15 @@ def transcode(src, output):
     for output_file in output:
         if output_file["type"] == "audio":
             args += ["-vn"]
-            args += ["-ar", "44100"]
+            args += ["-ar", 44100]
             args += ["-acodec", "copy"]
             args += ["-ac", 2]
             args += ["-ab", 192000]
             args += [output_file["path"]]
+        elif output_file["type"] == "video":
+            args += ["-b:v", output_file["bitrate"] * 1000]
+            args += "-f mpegts -acodec libfaac -ar 48000 -ab 64k -vcodec libx264 -flags +loop -cmp +chroma -subq 5 -trellis 1 -refs 1 -coder 0 -me_range 16 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -bt 200k -maxrate 96k -bufsize 96k -rc_eq 'blurCplx^(1-qComp)' -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -level 30 -aspect 320:240 -g 30 -s 320x240".split(' ')
+            args += [output_file["path"]]
         else:
-            raise NotImplementedError
+            raise NotImplementedError("Unsupported type")
     ffmpeg(args)
--- tests/test_transcode.py
+++ tests/test_transcode.py
@@ -9,3 +9,22 @@ def test_extracts_audio_from_ts(tmpdir):
     info = mediainfo(str(output_path))
     assert 'Audio Data Transport Stream' in info
     assert 'Advanced Audio Codec' in info
+
+def test_transcode_video_and_audio_from_ts(tmpdir):
+    audio_output_path = tmpdir.join("output.aac")
+    video_output_path = tmpdir.join("tvglobo_200.ts")
+    transcode(src="tests/data/tvglobo.ts", output=[
+        {"path": str(video_output_path), "type": "video", "bitrate": 200, "path": video_output_path},
+        {"path": str(audio_output_path), "type": "audio"}
+    ])
+
+    assert audio_output_path.check()
+    assert video_output_path.check()
+
+    audio_info = mediainfo(str(audio_output_path))
+    assert 'Audio Data Transport Stream' in audio_info
+    assert 'Advanced Audio Codec' in audio_info
+
+    video_info = mediainfo(str(video_output_path))
+    assert 'MPEG-TS' in video_info
+    assert 'Advanced Video Codec' in video_info
