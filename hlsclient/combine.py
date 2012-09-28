ef924a9c6731029ac5b7d74e260634df0916ff84
Juarez Bochi <jbochi@gmail.com>
2012-09-28 11:49:09 -0300

    Refactor variant playlist generation

--- hlsclient/combine.py
+++ hlsclient/combine.py
@@ -7,14 +7,21 @@ def combine_playlists(playlists, path):
     actions = playlists.get('actions', {})
     combine_actions = [action for action in actions if action['type'] == 'combine']
     for action in combine_actions:
-        generate_variant_playlist(action, playlists, path)
+        dump_variant_playlist(playlists, action, path)
 
-def generate_variant_playlist(action, playlists, path):
+def dump_variant_playlist(playlists, action, path):
+    m3u8 = generate_variant_playlist(playlists, action)
+    m3u8.dump(path + action['output'])
+
+def generate_variant_playlist(playlists, action):
     variant_m3u8 = m3u8.M3U8()
     for playlist_name in action['input']:
-        playlist_data = playlists['streams'][playlist_name]
-        bandwidth = str(playlist_data['bandwidth'])
-        m3u8_uri = playlist_data['input-path']
-        playlist = m3u8.Playlist(m3u8_uri, stream_info={'bandwidth': bandwidth, 'program_id': '1'}, baseuri="")
+        playlist = generate_single_playlist(playlists, playlist_name)
         variant_m3u8.add_playlist(playlist)
-    variant_m3u8.dump(path + action['output'])
+    return variant_m3u8
+
+def generate_single_playlist(playlists, playlist_name):
+    playlist_data = playlists['streams'][playlist_name]
+    bandwidth = str(playlist_data['bandwidth'])
+    m3u8_uri = playlist_data['input-path']
+    return m3u8.Playlist(m3u8_uri, stream_info={'bandwidth': bandwidth, 'program_id': '1'}, baseuri="")
