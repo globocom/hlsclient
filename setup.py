944d19f9ae1bc65853a606cabf0da668b5da880a
Hugo Lopes Tavares <hltbra@gmail.com>
2013-01-11 11:02:32 -0200

    Bump to 0.4.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=README,
     author='Globo.com',
     url='https://github.com/globocom/hlsclient',
-    version='0.4.7',
+    version='0.4.8',
     zip_safe=False,
     include_package_data=True,
     packages=[
